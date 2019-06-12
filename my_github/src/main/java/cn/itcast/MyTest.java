25a0d991a33075ebd806e4a77f282e861e94db39
weixin <619154987@qq.com>
2019-06-12 17:56:44 +0800

    12132131

---
+++ my_github/src/main/java/cn/itcast/MyTest.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+import javax.sound.midi.Soundbank;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
